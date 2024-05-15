8bb8e75580fdabfefccdfab2f28e05f7f0427f9f
Radzivon-Kliuchnikau <rodionk510@gmail.com>
2024-05-15 22:13:47 +0200

    Add Slack Request method

--- TimerTrigger1.cs
+++ TimerTrigger1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
@@ -14,13 +15,29 @@ namespace Company.Function
         }
 
         [Function("TimerTrigger1")]
-        public void Run([TimerTrigger("0 */1 * * * *")] TimerInfo myTimer)
+        public async Task Run([TimerTrigger("0 */1 * * * *")] TimerInfo myTimer)
         {
             _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
             
+            await MakeSlackRequest("Hello from Azure Function");
+
             if (myTimer.ScheduleStatus is not null)
             {
                 _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+            }
+        }
+
+        public static async Task<string> MakeSlackRequest(string message)
+        {
+            using (var client = new HttpClient())
+            {
+                var requestData = new StringContent("{'text':'" + message + "'}", Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync($"https://hooks.slack.com/services/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", requestData);
+
+                var result = await response.Content.ReadAsStringAsync();
+
+                return result;
             }
         }
     }
